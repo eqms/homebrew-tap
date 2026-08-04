0696ef225371ac66bea2d0e88b765749b0f0163d
Martin Schmid <m.schmid@equitania.de>
2026-08-04 16:57:22 +0200

    [CHG] actop v0.7.0: update formula

--- Formula/actop.rb
+++ Formula/actop.rb
@@ -1,8 +1,8 @@
 class Actop < Formula
   desc "Top-like terminal UI for Apple containers (like ctop)"
   homepage "https://github.com/eqms/actop"
-  url "https://github.com/eqms/actop/archive/refs/tags/v0.6.1.tar.gz"
-  sha256 "b023a6f04486c0cd1e3cc457481c2092305f4fd19ae4009ee80b19614c7f2340"
+  url "https://github.com/eqms/actop/archive/refs/tags/v0.7.0.tar.gz"
+  sha256 "70aeff57a1b4f53c0ab42d521870620a2d708c1049d3b311747f31edb86b62cc"
   license "MIT"
   head "https://github.com/eqms/actop.git", branch: "main"
 
